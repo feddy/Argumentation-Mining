dedd1024d7991af12c448adac28a4e043f6dd866
feddy <feddy86@gmail.com>
2020-08-13 21:51:07 +0700

    Add files via upload

---
+++ progs/BiLSTM-CNN-CRF/LasagneNLP-master/run_bi-lstm-cnn.sh
@@ -0,0 +1,10 @@
+THEANO_FLAGS='floatX=float32' python bi_lstm_cnn.py --fine_tune --embedding glove --oov embedding --update momentum \
+ --batch_size 10 --num_units 100 --num_filters 20 --learning_rate 0.1 --decay_rate 0.1 --grad_clipping 3 --regular none --dropout \
+ --train "data/conll2003/eng.train.bioes.conll" --dev "data/conll2003/eng.dev.bioes.conll" --test "data/conll2003/eng.test.bioes.conll" \
+ --embedding_dict "data/glove/glove.6B/glove.6B.100d.gz" --output_prediction --patience 5
+
+
+
+
+
+python2.7 bi_lstm_cnn.py --fine_tune --embedding glove --oov embedding --update momentum --batch_size 10 --num_units 100 --num_filters 20 --learning_rate 0.1 --decay_rate 0.1 --grad_clipping 3 --regular none --dropout  --train "data/conll/Essay_Level/train.dat" --dev "data/conll/Essay_Level/dev.dat" --test "data/conll/Essay_Level/test.dat" --embedding_dict "data/glove.6B.100d.gz" --output_prediction --patience 5
